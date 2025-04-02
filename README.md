55cf27c0aabae831e5e34ba78108a28622516ed7
Peterson Couto <peterson_fernando16@hotmail.com>
2025-04-02 18:20:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vue-bitcoin
+ Criar uma aplicação utilizando Vue 3 para consumir e exibir informações do Bitcoin em tempo real em português, utilizando a API CoinCap.
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="text-red-500 text-4xl font-bold ">Hello Tailwind!</h1>
+</template>
+
+<style>
+.read-the-docs {
+  color: #888;
+}
+</style>
