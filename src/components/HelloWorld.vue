b6c2df7c3c8c288d788265478186b7cad2be5620
Peterson Couto <peterson_fernando16@hotmail.com>
2025-04-02 18:28:25 -0300

    Correção de error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+defineProps<{ msg: string }>();
 </script>
 
 <template>
-  <h1 class="text-red-500 text-4xl font-bold ">Hello Tailwind!</h1>
+  <h1 class="text-red-500 text-4xl font-bold">Hello Tailwind!</h1>
 </template>
 
-<style>
+<style scoped>
 .read-the-docs {
   color: #888;
 }
